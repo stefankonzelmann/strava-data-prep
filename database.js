cff4a46956862cac88a4b2ed506fb979a9d6aa31
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2024-02-19 09:04:48 +0100

    Clean Up Log

--- database.js
+++ database.js
@@ -6,7 +6,6 @@ const { ATLAS_URI } = process.env;
 const client = new MongoClient(ATLAS_URI);
 
 export async function insertActivities(activities) {
-  console.log("Function insert activites: ", activities);
   console.log(
     `Received ${activities.length} activities from Strava API for inserting to database`
   );
